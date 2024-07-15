d52e5d0e6f040c51179b693705dff2f1fa90095f
Fer474 <ferchacon474@gmail.com>
2024-07-15 14:06:00 -0600

    Functional login

--- src/test/java/appiumtests/tests/Tests.java
+++ src/test/java/appiumtests/tests/Tests.java
@@ -5,14 +5,27 @@ import org.junit.Test;
 
 public class Tests extends Base {
     @Test
-    @Description("In this test we move a champion into the board")
+    @Description("In this test login ")
     public void Tutorial(){
+        loginPage.loginWith("Riot");
+        loginPage.loginAs("fersh474").withPassword("Avengers3..");
+        loginPage.logIn();
         menuPage.play()
                 .wihGameMode("tutorial");
         inGamePage.champ1board()
                 .buyChamp()
                 .champ2board();
     }
+
+//    @Test
+//    @Description("In this test we move a champion into the board")
+//    public void Tutorial(){
+//        menuPage.play()
+//                .wihGameMode("tutorial");
+//        inGamePage.champ1board()
+//                .buyChamp()
+//                .champ2board();
+//    }
 
 //    @Test
 //    @Description("In this test we start an online game")
