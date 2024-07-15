86e4182b4b489a3316fc03313e99e81753e8f76b
Fer474 <ferchacon474@gmail.com>
2024-07-15 15:37:35 -0600

    Functional login

--- src/test/java/appiumtests/pages/InGame.java
+++ src/test/java/appiumtests/pages/InGame.java
@@ -4,18 +4,16 @@ public class InGame extends Common{
     public InGame champ1board(){
         try {
             Thread.sleep(20000); // Pausa durante 1000 milisegundos (1 segundo)
-        } catch (InterruptedException e) {
-            // Manejo de excepciones aquí
-        }
+        } catch (InterruptedException e) {}
         moveElement(601,962,1203,717,1);
+        System.out.println("a");
         return this;
     }
+
     public InGame champ2board(){
         try {
             Thread.sleep(2000); // Pausa durante 1000 milisegundos (1 segundo)
-        } catch (InterruptedException e) {
-            // Manejo de excepciones aquí
-        }
+        } catch (InterruptedException e) {}
         moveElement(601,962,1511,717,1);
         return this;
     }
