18e87a2aa45fa3d0ebaa7c5c51ddb55492cfcb5f
Fer474 <ferchacon474@gmail.com>
2024-07-22 17:10:51 -0600

    Auto Adb added

--- src/main/java/org/tft/Assertions.java
+++ src/main/java/org/tft/Assertions.java
@@ -1,7 +1,35 @@
 package org.tft;
 
+import com.aspose.ocr.AsposeOCR;
 import org.junit.Assert;
+import org.tft.conections.Actions;
 
 public class Assertions {
+    public void assert1 (){
+        // ExStart:1
+        // The path to the documents directory.
+        String dataDir = Utils.getSharedDataDir(Actions.class);
 
+        // The image path
+        String imagePath1 = dataDir + "img.png";
+        String imagePath2 = dataDir + "img1.png";
+
+        //Create api instance
+        AsposeOCR api = new AsposeOCR();
+
+        String textToFind = "Kha";
+
+        // Detect if image has text
+        boolean result = api.ImageHasText(imagePath1, textToFind);
+        System.out.println("Image Has Text: "+ textToFind + " : " + result);
+
+        // Compare two images by texts
+        boolean isEqual = api.CompareImageTexts(imagePath1, imagePath2);
+        System.out.println("Images are equal: " + isEqual);
+
+        // Compare two images by texts
+        float diff = api.ImageTextDiff(imagePath1, imagePath2);
+        System.out.println("Difference: " + diff);
+        // ExEnd:1
+    }
 }
